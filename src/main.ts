aa0ee06eabb6f58548f6b6d81588c2a8ee602f75
Kim Myungseong <forwarm5891@gmail.com>
2022-11-15 23:12:42 +0900

    feat: boiler plate 작성 및 validationpipe 구현

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 필요로 하는 속성만 받을 수 있게 도와주는 whitelist 프로퍼티.(불필요한 속성 제거)
+      whitelist:true,
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
